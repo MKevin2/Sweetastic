34db25dc810684aef7cef7fc8db8a8957b33c80e
MatheusGamaRussi <matheus.russi@etec.sp.gov.br>
2023-06-21 21:56:00 -0300

    Criação da tela de Nome de Usuário

--- app/src/main/java/com/example/sweetastic/MainActivity.java
+++ app/src/main/java/com/example/sweetastic/MainActivity.java
@@ -5,13 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    static String mensagem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent nomeUsu = getIntent();
+        String msg = nomeUsu.getStringExtra(NomeActivity.NOME_USUARIO);
+        mensagem = "Seja bem vindo, " + msg + "!";
+        TextView nomeText = (TextView)findViewById(R.id.nome_text);
+        nomeText.setText(mensagem);
     }
 
     public void paraUsos(View view){
